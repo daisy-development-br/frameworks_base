e86c3a2af091815c6cf1685ab0116b119aa98ce1
Matt Pietal <mpietal@google.com>
2021-09-23 10:18:41 -0400

    [DO NOT MERGE] Only show lock icon background with UDFPS
    Otherwise the background should remain hidden (View.GONE) for devices
    without the UDFPS sensor.
    
    Fixes: 200813382
    Test: atest LockIconViewControllerTest
    Change-Id: I1ef3cef37de60e6968432869f4c946b6de414db4

--- packages/SystemUI/src/com/android/keyguard/LockIconView.java
+++ packages/SystemUI/src/com/android/keyguard/LockIconView.java
@@ -47,6 +47,7 @@ public class LockIconView extends FrameLayout implements Dumpable {
     private ImageView mBgView;
 
     private int mLockIconColor;
+    private boolean mUseBackground = false;
 
     public LockIconView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -60,8 +61,8 @@ public class LockIconView extends FrameLayout implements Dumpable {
         mBgView = findViewById(R.id.lock_icon_bg);
     }
 
-    void updateColorAndBackgroundVisibility(boolean useBackground) {
-        if (useBackground && mLockIcon.getDrawable() != null) {
+    void updateColorAndBackgroundVisibility() {
+        if (mUseBackground && mLockIcon.getDrawable() != null) {
             mLockIconColor = Utils.getColorAttrDefaultColor(getContext(),
                     android.R.attr.textColorPrimary);
             mBgView.setBackground(getContext().getDrawable(R.drawable.fingerprint_bg));
@@ -77,11 +78,22 @@ public class LockIconView extends FrameLayout implements Dumpable {
 
     void setImageDrawable(Drawable drawable) {
         mLockIcon.setImageDrawable(drawable);
+
+        if (!mUseBackground) return;
+
         if (drawable == null) {
             mBgView.setVisibility(View.INVISIBLE);
         } else {
             mBgView.setVisibility(View.VISIBLE);
         }
+    }
+
+    /**
+     * Whether or not to render the lock icon background. Mainly used for UDPFS.
+     */
+    public void setUseBackground(boolean useBackground) {
+        mUseBackground = useBackground;
+        updateColorAndBackgroundVisibility();
     }
 
     public void setCenterLocation(@NonNull PointF center, int radius) {
